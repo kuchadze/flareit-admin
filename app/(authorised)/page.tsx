8923c76fc7a6db45d0336b65a81ff6ca118a81d7
luka <luka.kuchadze25@gmail.com>
2024-08-19 19:20:07 +0400

    delete

--- app/(authorised)/page.tsx
+++ app/(authorised)/page.tsx
@@ -1,10 +1,5 @@
-import Header from '../Components/Header/Header';
 import styles from './page.module.scss';
 
 export default function Home() {
-    return (
-        <main className={styles.main}>
-            <Header />
-        </main>
-    );
+    return <main className={styles.main}></main>;
 }
