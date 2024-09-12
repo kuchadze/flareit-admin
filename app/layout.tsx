9e9605dae9d0b439bd073911feb37bd018a16699
luka <luka.kuchadze25@gmail.com>
2024-09-12 10:15:25 +0400

    add favicon in admin

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href="/images/MusicLogo.svg" />
+            </head>
             <body className={inter.className}>
                 <RecoilWrapper>{children}</RecoilWrapper>
             </body>
